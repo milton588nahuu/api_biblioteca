e357c8951b0c2fe67dfb2bc392d57153e6ecabdb
milton588nahu <milton.candia.244@mi.unc.edu.ar>
2023-12-15 23:07:53 -0300

    actividad 5 terminada

--- routes/libros.js
+++ routes/libros.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
 const Libro = require('../models/Libro');
-
 //Ruta para obtener todos los libros 
 router.get("/", async(req,res) => {
     try {
